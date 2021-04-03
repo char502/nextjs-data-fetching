1697738354bd95318c80330599a493314c57dc1b
charlotte@virtualtrips.io <char502@hotmail.com>
2021-04-03 17:43:56 +0100

    used getStaticProps example to fetch data

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,53 @@
+import path from 'path';
+// path is another node.js module; help for building paths
+import fs from 'fs/promises';
+// this imports the file system module from Node.js
+
 function HomePage(props) {
+
+  const { products } = props;
+
+  // console.log(products)
+
   return (
     <ul>
       <p>this is a test</p>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map(product => (
+        <li key={product.id}>
+          {product.title}
+        </li>
+      ))}
     </ul>
   );
 }
+
+export async function getStaticProps() {
+
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  // This builds an absolute path to the file: 'dummy-backend...'
+
+  // fs.readFileSync()
+  // synchronously reads a file and blocks app/file execution until it's done
+
+  const jsonData = await fs.readFile(filePath)
+  // readFile needs a callBack to continue
+  //when use fs from 'fs/promises', readFile returns a promise
+  // as this is an async function (as defined above) can then await this
+  // then just needs a path to the file want to read
+
+  const data = JSON.parse(jsonData)
+  // JSON.parse - reads the data and converts it into a regular js object
+
+  console.log(data.products)
+
+  return { 
+    props: {
+      products: data.products
+    },
+  }
+}
+// with this function, always need to return an object with a props key
+// this exposes data, through props, to the main component (HomePage in this example)
+// The data should be an object
 
 export default HomePage;
