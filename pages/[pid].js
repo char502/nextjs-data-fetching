c6343dcadf6f881d583dd7c515d99f76146eb694
charlotte@virtualtrips.io <char502@hotmail.com>
2021-04-07 22:46:29 +0100

    finished getStaticProps and getStaticPaths sections

--- pages/[pid].js
+++ pages/[pid].js
@@ -8,11 +8,11 @@ function ProductDetailPage(props) {
 
 const { loadedProduct } = props;
 
-  // if (!loadedProduct) {
-  //   return (
-  //     <p>Loading...</p>
-  //   )
-  // }
+  if (!loadedProduct) {
+    return (
+      <p>Loading...</p>
+    )
+  }
 
   return (
     <Fragment>
@@ -22,21 +22,37 @@ const { loadedProduct } = props;
   )
 }
 
-export async function getStaticProps(context) {
-
-  const { params } = context;
-
-  const productId = params.pid;
-  console.log(productId)
-
+async function getData() {
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
   const jsonData = await fs.readFile(filePath)
   const data = JSON.parse(jsonData)
 
+  return data;
+}
+
+export async function getStaticProps(context) {
+
+  const { params } = context;
+  // console.log(params)
+
+  const productId = params.pid;
+  // console.log(productId)
+
+  const data = await getData();
+  // console.log(data.products)
+
   const product = data.products.find(product => product.id === productId);
+  // console.log(product)
 
   // Want to return true if the ID of the product we're currently looking at in that array is equal to the product id have here in our parameters
   // If this is equal, know that this is the product we need for this component
+
+  // if fail to find a product for a given id
+  if (!product) {
+    return (
+      { notFound : true }
+    )
+  }
 
   return {
     props: {
@@ -47,11 +63,21 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+
+  const data = await getData();
+
+  const ids = data.products.map((product) => product.id);
+  // console.log(ids)
+
+  const pathsWithParams = ids.map((id) => ({ params: {'pid': id} }));
+  // console.log(pathsWithParams)
+
+  // This puts the data in the same format as the dummy-backend.json file which is more realistic as are unlikely to hard code all supported values ahead of time as won't know what they are 
+  // But would fetch them from the same data source as the actual data so emulate the same shape as that data
+
   return {
-    paths: [
-      { params: { pid: "p1"} }
-    ],
-    fallback: 'blocking'
+    paths: pathsWithParams,
+    fallback: true
   };
 }
 
