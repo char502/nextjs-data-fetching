6c3a80750fe39f658cc6764e3bd42744cdee9753
charlotte@virtualtrips.io <char502@hotmail.com>
2021-09-28 03:25:48 +0100

    training notes index and pid js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
-import path from 'path';
 // path is another node.js module; help for building paths
-import fs from 'fs/promises';
+import path from 'path';
 // this imports the file system module from Node.js
+// (one of core) Node.js modules
+// would fail if tried to use it on the client side
+// will also be stripped out of the client side code bundle
+import fs from 'fs/promises';
+
 import Link from 'next/link';
 
 function HomePage(props) {
-
   const { products } = props;
 
   // console.log(products)
@@ -13,47 +16,104 @@ function HomePage(props) {
   return (
     <ul>
       <p>this is a test</p>
-      {products.map(product => (
+      {products.map((product) => (
         <li key={product.id}>
-          <Link href={`/products/${product.id}`}>
-            {product.title}
-          </Link>
+          <Link href={`/products/${product.id}`}>{product.title}</Link>
         </li>
       ))}
     </ul>
   );
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(context) {
+  // can only use on page component files
+  // need to export it to make NextJS aware of it
+  // executes when build your project (with Next build)
+  // can be an issue for data constantly changing (will become outdated quickly)
+  // would need to use useEffect for when any data changes/ fetching updated data from a server
+  // however, can use Incremental Static Generation (ISR)
 
-  console.log('(Re-)Generating...')
+  // With the developement server, will always see the latest page with the latest data
+  // So if re load this page this console.log will show each time to prove that is happening
+  // It will ignore the revalidate/ISR, it will run every time
+  console.log('(Re-)Generating...');
+
+  // ==============
+  // context object
+  // with GSP get a context parameter,
+  // get dynamic params/path segments/values
+  // ==============
 
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
   // This builds an absolute path to the file: 'dummy-backend...'
+  // process object is globally available in node.js
+  // process.cwd refers to the current working directory of this code file when it is executed
+  // this cwd will not be the pages folder
+  // when this file is executed, Next.js will be executing it
+  // Next.js will treat all the files as if they sit in the root project folder
+  // so cwd in this case will be the overall project folder instead of the pages folder
 
   // fs.readFileSync()
   // synchronously reads a file and blocks app/file execution until it's done
 
-  const jsonData = await fs.readFile(filePath)
+  const jsonData = await fs.readFile(filePath);
   // readFile needs a callBack to continue
   //when use fs from 'fs/promises', readFile returns a promise
   // as this is an async function (as defined above) can then await this
   // then just needs a path to the file want to read
 
-  const data = JSON.parse(jsonData)
+  const data = JSON.parse(jsonData);
+  // JSON is a globally available object (just as it is on the browser side)
   // JSON.parse - reads the data and converts it into a regular js object
 
   // console.log(data.products)
 
-  return { 
+  // ===============================
+  // Not found example
+  // ===============================
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
+
+  // ===============================
+  // Redirect example
+  // ===============================
+  // if (!data) {
+  //   return {
+  //     redirect: {
+  //       destination: '/no-data'
+  //     }
+  //   }
+  // }
+
+  return {
     props: {
       products: data.products
     },
+    // This enables Incremental Static Generation (ISR)
+    // the number is the time in seconds that NextJS should wait until it regenerates the page
+    // for development the page will be regenerated for every request no matter what is entered here
+    // so with the developement server, will always see the latest page with the latest data
+    // In production, this number will matter
     revalidate: 10
-  }
+
+    // ============================================
+    // also get other keys:
+    // -----------
+    // notFound: expects a boolean value
+    // -----------
+    // if true, will render the 404 error page instead of the normal page
+    // can possibly use it if there's no data to fetch (as per example above)
+    // -----------
+    // redirect
+    // useful option if something goes wrong with fetching your data
+    // ============================================
+  };
 }
 // with this function, always need to return an object with a props key
 // this exposes data, through props, to the main component (HomePage in this example)
 // The data should be an object
+
+// The server side code doesen't actually run on the server, serving the application, it runs on the local machine when running the NextJs build script
 
 export default HomePage;
--- pages/products/[pid].js
+++ pages/products/[pid].js
@@ -1,17 +1,13 @@
-
 import { Fragment } from 'react';
 
 import path from 'path';
 import fs from 'fs/promises';
 
 function ProductDetailPage(props) {
-
-const { loadedProduct } = props;
+  const { loadedProduct } = props;
 
   if (!loadedProduct) {
-    return (
-      <p>Loading...</p>
-    )
+    return <p>Loading...</p>;
   }
 
   return (
@@ -19,29 +15,42 @@ const { loadedProduct } = props;
       <h1>{loadedProduct.title}</h1>
       <p>{loadedProduct.description}</p>
     </Fragment>
-  )
+  );
 }
+
+// if use useEffect, there will be no data there when the page is initially rendered
+// so search engines wouldn't see it
 
 async function getData() {
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
-  const jsonData = await fs.readFile(filePath)
-  const data = JSON.parse(jsonData)
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
 
   return data;
 }
 
 export async function getStaticProps(context) {
-
   const { params } = context;
-  // console.log(params)
+  // console.log(params); // this returns an object,
+  // an example, value may be p1, p2 pr p3 { pid: 'p2' }
 
   const productId = params.pid;
-  // console.log(productId)
+  // console.log(productId); // example, value may be p1, p2 pr p3
+  // can also extract params in the useRouter hook inside the component
+  // when extract params in the component function, can use them inside the component
+  // for example, use extracted id to send the request to some back end server to fetch
+  // data from there
+  // but that would only happen in the browser
+  // =========================================
+  // If want to pre-render a page (with the help of Get Static Props) then need to do
+  // this on the server
+  // Get Static Props runs before the component runs so use this way of extracting params
+  // before the page renders
 
   const data = await getData();
   // console.log(data.products)
 
-  const product = data.products.find(product => product.id === productId);
+  const product = data.products.find((product) => product.id === productId);
   // console.log(product)
 
   // Want to return true if the ID of the product we're currently looking at in that array is equal to the product id have here in our parameters
@@ -49,9 +58,7 @@ export async function getStaticProps(context) {
 
   // if fail to find a product for a given id
   if (!product) {
-    return (
-      { notFound : true }
-    )
+    return { notFound: true };
   }
 
   return {
@@ -62,22 +69,53 @@ export async function getStaticProps(context) {
   };
 }
 
+// NextJs pre generates pages by default
+// This is not the case if you have a [dynamic] page like this one
+// Then the default behaviour is to not pre generate the page
+// because it is a dynamic page so technically may repesent many possible pages
+// Next doesn't know in advance which one you are going to want
+// Therefore NextJS always generates them 'Just in time' on the server (once it knows which one you want)
+// ===========================================================
+// For dynamic routes, have to give NextJs more information
+// Therefore have to use the following
 // for dynamic paths that need to be pre-generated
-export async function getStaticPaths() {
 
+// This tells NextJS which paths/instances of a dynamic page should be pre-generated
+export async function getStaticPaths() {
+  // need to export GSPaths to make NextJS aware of it
+  // Like GSP and GSSP, can only use GSP on page component files
   const data = await getData();
 
   const ids = data.products.map((product) => product.id);
-  // console.log(ids)
+  // console.log(ids);
 
-  const pathsWithParams = ids.map((id) => ({ params: {'pid': id} }));
-  // console.log(pathsWithParams)
+  const pathsWithParams = ids.map((id) => ({ params: { pid: id } }));
+  // console.log(pathsWithParams);
 
-  // This puts the data in the same format as the dummy-backend.json file which is more realistic as are unlikely to hard code all supported values ahead of time as won't know what they are 
+  // This puts the data in the same format as the dummy-backend.json file which is more realistic as are unlikely to hard code all supported values ahead of time as won't know what they are
   // But would fetch them from the same data source as the actual data so emulate the same shape as that data
 
   return {
     paths: pathsWithParams,
+    // fallback is helpful if have a lot of pages that would need to be pre generated
+    // i.e. Amazon, may have thousands of pages
+    // pre rendering them all might not be optimal
+    // fallback: true, tells NextJS that even pages that are not listed here
+    // can be valid when they are visited
+    // they are just not pre-generated, just generated JIT when a request reaches the server
+    // still need to prepare a fallback state in your component
+    // i.e.
+    // if (!loadedProduct) {
+    //   return <p>Loading...</p>;
+    // }
+    // an alternative would be to set fallback to a string with a value of 'blocking'
+    // Then don't need to fallback check
+    // because then NextJs will wait for the page to be fully generated on the server
+    // before the server serves that
+    // this will take a little bit longer for the visitor to the page to get a response
+    // but the response that is sent back, will be finished
+    // then just need to decide which approach need to take, whether need something
+    // quickly on the page for the user or it's ok to wait
     fallback: true
   };
 }
